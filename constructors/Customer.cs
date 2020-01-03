00102b1674f4571d3a176894b276da25ab7505d2
unknown <samhuynhle@gmail.com>
2020-01-03 01:21:12 -0800

    default/paramarless constructors, overloading constructors, and how to use this keyword for constructors added in constructors code

--- constructors/Customer.cs
+++ constructors/Customer.cs
@@ -11,23 +11,29 @@ namespace constructors
         // whenever we have a class with a list of objects, we always initialize the list as empty
 
         // fast way, type 'ctor', try to not type every letter
-        public Customer(int id)
-        {
-            this.Id = id;
-            Orders = new List<Order>();
-        }
-        // overload with different signature
-        public Customer(int id, string name)
-        {
-            this.Id = id;
-            this.Name = name;
-            Orders = new List<Order>();
-        }
         // paramerless
         public Customer()
         {
             Orders = new List<Order>();
         }
+
+        // how do we avoid copying pasting the Orders initialize into other constructor methods?
+        // we can chain constructors with the ': this()' set up
+        // in general not a good practice as it does add some more complexity
+        // so use ': this()' limited, use it when we really have to use initialization, this case the List
+        public Customer(int id)
+            : this()
+        {
+            this.Id = id;
+        }
+        // overload with different signature
+        public Customer(int id, string name)
+            : this(id)
+        {
+            this.Name = name;
+        }
+
+
 
         // only use constructors in classes when we really want to initialize the class
     }

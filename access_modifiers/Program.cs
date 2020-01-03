69d4d1655f02da9f08e8f1dee9928443708306eb
unknown <samhuynhle@gmail.com>
2020-01-03 02:37:18 -0800

    used getter/setter and private fields

--- access_modifiers/Program.cs
+++ access_modifiers/Program.cs
@@ -7,10 +7,22 @@ namespace access_modifiers
         public class Person
         {
             private DateTime _birthdate; // with private we can't access this field outside the Person class
+
+            public void SetBirthdate(DateTime birthdate)
+            {
+                // implement logic for birthdate
+                _birthdate = birthdate;
+            }
+            public DateTime GetBirthdate()
+            {
+                return _birthdate;
+            }
         }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var person = new Person();
+            person.SetBirthdate(new DateTime(1995, 08, 31));
+            System.Console.WriteLine(person.GetBirthdate());
         }
     }
 }

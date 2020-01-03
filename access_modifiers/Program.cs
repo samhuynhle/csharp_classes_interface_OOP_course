ffa36322c8749fcf61722cfd392683530e75a6f7
unknown <samhuynhle@gmail.com>
2020-01-03 02:34:27 -0800

    access modifiers code started

---
+++ access_modifiers/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace access_modifiers
+{
+    class Program
+    {
+        public class Person
+        {
+            private DateTime _birthdate; // with private we can't access this field outside the Person class
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
